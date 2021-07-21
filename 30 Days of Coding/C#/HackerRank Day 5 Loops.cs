6859b5412ea871e1a2496ce5d5efdd5b5a105a2d
Jay Pankhania <pankhaniyajay@gmail.com>
2021-07-21 19:28:08 +0530

    Updated the Code for the HackerRank Day 5 Loops

--- 30 Days of Coding/C#/HackerRank Day 5 Loops.cs
+++ 30 Days of Coding/C#/HackerRank Day 5 Loops.cs
@@ -12,18 +12,39 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-public class Solution
+class Solution
 {
     public static void Main(string[] args)
     {
-        int input = Convert.ToInt32(Console.ReadLine().Trim());
-        calcTable(input);
+        printTableWithFor(Convert.ToInt32(Console.ReadLine().Trim()));
+        // printTableWithWhile(Convert.ToInt32(Console.ReadLine().Trim()));
+        // printTableWithDoWhile(Convert.ToInt32(Console.ReadLine().Trim()));
     }
-    public static void calcTable(int input)
+    public static void printTableWithFor(int n)
     {
         for (int i = 1; i <= 10; i++)
         {
-            Console.WriteLine(input + " x " + i + " = " + (input * i));
+            Console.WriteLine(n + " x " + i + " = " + (n * i));
         }
+    }
+    public static void printTableWithWhile(int n)
+    {
+        int i=1;
+        while(i <= 10)
+        {
+            Console.WriteLine(n + " x " + i + " = " + (n * i));
+            i++;
+        }
+    }
+    public static void printTableWithDoWhile(int n)
+    {
+        int i=0;
+        do
+        {
+            i++;
+            if(i>0 && i<=10)
+                  Console.WriteLine(n + " x " + i + " = " + (n * i));
+
+        }while(i <= 10);
     }
 }
