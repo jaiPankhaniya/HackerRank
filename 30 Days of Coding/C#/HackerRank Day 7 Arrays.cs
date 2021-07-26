7b95972ddaa0366e1c9a1ddac36d737a96bafba0
Jay Pankhania <pankhaniyajay@gmail.com>
2021-07-26 11:25:25 +0530

    applied change to HackerRank Day 7 Arrays

--- 30 Days of Coding/C#/HackerRank Day 7 Arrays.cs
+++ 30 Days of Coding/C#/HackerRank Day 7 Arrays.cs
@@ -16,10 +16,20 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        int n = Convert.ToInt32(Console.ReadLine().Trim());
-        string[] arr_temp = Console.ReadLine().Split(' ');
+        //commented code is for performing the operations for the dynamic number of arrays.
+        /*int numOfArray = Conver.ToInt32(Console.ReadLine().Trim());
+        for(int i = 0;i<=numOfArray;i++)
+        {
+           int n = Convert.ToInt32(Console.ReadLine().Tim());
+           string[] arr_temp[] = Console.ReadLine().Split(' ');
+           array(arr_temp,n);
+        } */
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());		//Taking Number of elements for Array
+        string[] arr_temp = Console.ReadLine().Split(' ');      //Taking elements of Array
         array(arr_temp,n);
     }
+	//Function to print array in reverse.
     public static void array(string[] tmp_arr,int n)
     {
         int[] arr = Array.ConvertAll(tmp_arr,Int32.Parse);
