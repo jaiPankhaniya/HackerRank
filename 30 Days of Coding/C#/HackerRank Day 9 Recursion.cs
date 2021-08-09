2c368268c987acf96dafeea53b57d119b5f4de1b
Jay Pankhania <pankhaniyajay@gmail.com>
2021-08-09 12:58:02 +0530

    Updated the HackerRank Day 9 Recursion

--- 30 Days of Coding/C#/HackerRank Day 9 Recursion.cs
+++ 30 Days of Coding/C#/HackerRank Day 9 Recursion.cs
@@ -14,13 +14,6 @@ using System;
 
 class Result
 {
-    /*
-     * Complete the 'factorial' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER n as parameter.
-     */
-
     public static int factorial(int n)
     {
         if (n == 1) return 1;
@@ -32,16 +25,8 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        TextWriter textWriter = new StreamWriter
-        (@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        int result = Result.factorial(n);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+        Console.WriteLine(Result.factorial(n));
     }
 }
